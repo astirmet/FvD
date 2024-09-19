5b76c876f5249cade3aab076c369946a5620b71b
astirmet <118255404+astirmet@users.noreply.github.com>
2024-09-19 16:22:27 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,21 @@
 // JavaScript Document
 console.log("hi");
+
+
+// JavaScript om de header tekst elke 5 seconden te veranderen
+// REMINDER MEHMET JAAR 2 JAVASCRIPT Const zorgt ervoor dat je de variabel-
+// zelf niet kan veranderen maar het object daarin wel zoals text etc.
+
+const texts = ["DADDY'S HOME", "10% STUDENTENKORTING", "GRATIS STANDAARD LEVERING VANAF $70",
+    "BINNEN 30 DAGEN GRATIS RETOURENEREN. *UITZONDERINGEN VAN TOEPASSING"
+];
+
+// REMINDER JAAR 2 Function 
+function changeText() {
+    const h1 = document.getElementById("header-text");
+    index = (index + 1) % texts.length;
+    headerText.textContent = texts[index];
+}
+
+// Verander de tekst elke 5 seconden
+setInterval(changeText, 5000);

4379d7e0bfdbcbc4e29d28146a838d6a3ce0e71b
astirmet <118255404+astirmet@users.noreply.github.com>
2024-10-03 18:20:17 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -6,16 +6,50 @@ console.log("hi");
 // REMINDER MEHMET JAAR 2 JAVASCRIPT Const zorgt ervoor dat je de variabel-
 // zelf niet kan veranderen maar het object daarin wel zoals text etc.
 
-const texts = ["DADDY'S HOME", "10% STUDENTENKORTING", "GRATIS STANDAARD LEVERING VANAF $70",
-    "BINNEN 30 DAGEN GRATIS RETOURENEREN. *UITZONDERINGEN VAN TOEPASSING"
-];
+// const texts = ["DADDY'S HOME", "10% STUDENTENKORTING", "GRATIS STANDAARD LEVERING VANAF $70",
+//     "BINNEN 30 DAGEN GRATIS RETOURENEREN. *UITZONDERINGEN VAN TOEPASSING"
+// ];
 
-// REMINDER JAAR 2 Function 
-function changeText() {
-    const h1 = document.getElementById("header-text");
-    index = (index + 1) % texts.length;
-    headerText.textContent = texts[index];
+// // REMINDER JAAR 2 Function 
+// function changeText() {
+//     const h1 = document.getElementById("header-text");
+//     index = (index + 1) % texts.length;
+//     headerText.textContent = texts[index];
+// }
+// // Verander de tekst elke 5 seconden
+// setInterval(changeText, 5000);
+
+
+
+
+
+// const menuOpenButton = document.querySelector("li > button");
+// const deNav = document.querySelector("header nav");
+
+
+
+// menuOpenButton.onclick = openMenu;
+
+
+// function openMenu(){
+//   deNav.classList.add("toonMenu")
+  
+// }
+
+
+// sluiten button
+
+const menuOpen = document.querySelector("header button");
+const navMenu = document.querySelector('header nav')
+
+
+menuOpen.addEventListener('click', openMenu)
+
+
+function openMenu(){
+    navMenu.classList.add('toonMenu')
 }
 
-// Verander de tekst elke 5 seconden
-setInterval(changeText, 5000);
+function sluitMenu() {
+  deNav.classList.remove("toonMenu");
+}
